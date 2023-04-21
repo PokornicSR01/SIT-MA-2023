b92add4f261c160e945106a04e0123a536a44890
PokornicSR01 <nemanja.pokornic02@gmail.com>
2023-04-21 19:20:04 +0200

    add fragments package

--- app/src/main/java/com/example/slagalica/fragments/SecondFragment.java
+++ app/src/main/java/com/example/slagalica/fragments/SecondFragment.java
@@ -1,4 +1,4 @@
-package com.example.slagalica;
+package com.example.slagalica.fragments;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -9,6 +9,7 @@ import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.fragment.NavHostFragment;
 
+import com.example.slagalica.R;
 import com.example.slagalica.databinding.FragmentSecondBinding;
 
 public class SecondFragment extends Fragment {
