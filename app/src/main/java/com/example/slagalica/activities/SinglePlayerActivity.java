bf5411b1c4fc3f2418f07d82c6996f767a4933a1
PokornicSR01 <nemanja.pokornic02@gmail.com>
2023-04-22 22:23:18 +0200

    feat: spajanje activitia

--- app/src/main/java/com/example/slagalica/activities/SinglePlayerActivity.java
+++ app/src/main/java/com/example/slagalica/activities/SinglePlayerActivity.java
@@ -2,7 +2,9 @@ package com.example.slagalica.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 import com.example.slagalica.R;
 
@@ -12,5 +14,35 @@ public class SinglePlayerActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_single_player);
+
+        Button buttonKoZnaZnaActivity = findViewById(
+                R.id.button_koznazna_singleplayer
+        );
+        buttonKoZnaZnaActivity.setOnClickListener(view -> {
+            Intent secondActivityIntent = new Intent(
+                    getApplicationContext(), KoZnaZna.class
+            );
+            startActivity(secondActivityIntent);
+        });
+
+        Button buttonMojBrojActivity = findViewById(
+                R.id.button_mojbroj_singleplayer
+        );
+        buttonMojBrojActivity.setOnClickListener(view -> {
+            Intent secondActivityIntent = new Intent(
+                    getApplicationContext(), MojBrojActivity.class
+            );
+            startActivity(secondActivityIntent);
+        });
+
+        Button buttonSpojniceActivity = findViewById(
+                R.id.button_spojnice_singleplayer
+        );
+        buttonSpojniceActivity.setOnClickListener(view -> {
+            Intent secondActivityIntent = new Intent(
+                    getApplicationContext(), SpojniceSinglePlayerActivity.class
+            );
+            startActivity(secondActivityIntent);
+        });
     }
 }
