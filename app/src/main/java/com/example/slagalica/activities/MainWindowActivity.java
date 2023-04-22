7a637ae5d10dddb0cd615a2c0745f14ada1f97ff
PokornicSR01 <nemanja.pokornic02@gmail.com>
2023-04-22 22:13:00 +0200

    add: MojBroj, SinglePlayerActivity, SpojniceSinglePlayerActivity

--- app/src/main/java/com/example/slagalica/activities/MainWindowActivity.java
+++ app/src/main/java/com/example/slagalica/activities/MainWindowActivity.java
@@ -1,13 +1,16 @@
 package com.example.slagalica.activities;
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import com.example.slagalica.MainActivity;
 import com.example.slagalica.fragments.FirstFragment;
 import com.google.android.material.snackbar.Snackbar;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.view.View;
+import android.widget.Button;
 
 import androidx.core.view.WindowCompat;
 import androidx.navigation.NavController;
@@ -26,6 +29,19 @@ public class MainWindowActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_window);
+
+        Button button_cancel = (Button) findViewById(R.id.button_singleplayer);
+        button_cancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                button_cancel.setOnClickListener(view -> {
+                    Intent secondActivityIntent = new Intent(
+                            getApplicationContext(), SinglePlayerActivity.class
+                    );
+                    startActivity(secondActivityIntent);
+                });
+            }
+        });
 
     }
 
---
+++ app/src/main/java/com/example/slagalica/activities/SinglePlayerActivity.java
@@ -0,0 +1,16 @@
+package com.example.slagalica.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.slagalica.R;
+
+public class SinglePlayerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_single_player);
+    }
+}
