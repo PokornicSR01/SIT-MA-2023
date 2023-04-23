96e0c6428b17583edfe449efa3d1a6df7e2b7c94
zankyosanka <aleksakuzmanovic94@gmail.com>
2023-04-23 20:28:08 +0200

    add: asocijacije,korakpokorak,skocko

---
+++ app/src/main/java/com/example/slagalica/activities/SkockoSinglePlayerActivity.java
@@ -0,0 +1,16 @@
+package com.example.slagalica.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.slagalica.R;
+
+public class SkockoSinglePlayerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_skocko_single_player);
+    }
+}
